185ebc84194343ab115dacab08b079f4d829100b
Kevin R. <kevinrc96@hotmail.com>
2023-11-14 21:29:06 +0100

    Ejercicio terminado y ejercicio resuelto por Data abajo

--- js/script.js
+++ js/script.js
@@ -66,3 +66,29 @@ generoAventura.forEach(pelicula => {
 
   genero12Div.appendChild(peliculaDiv);
 })
+
+//Ejercicio resuelto debajo ------------------------------------------>
+
+import peliculas from './peliculas.js'
+console.log(peliculas)
+
+const mostrarPeliculaPorGenero = (generoId, contenedorId) => {
+  const contenedor = document.getElementById(contenedorId)
+  const peliculaGenero = peliculas.filter((pelicula) => pelicula.genre_ids.includes(generoId))
+
+  peliculaGenero.forEach(pelicula => {
+    let templatePelis =`
+    <div class="pelicula">
+      <img src="https://image.tmdb.org/t/p/w200/${pelicula.backdrop_path}" alt="${pelicula.title}">
+      <h3>${pelicula.title}</h3>
+    </div>
+    `
+    contenedor.innerHTML += templatePelis
+  })
+
+}
+
+
+mostrarPeliculaPorGenero(28, "genero-28")
+mostrarPeliculaPorGenero(53, "genero-53")
+mostrarPeliculaPorGenero(12, "genero-12")
